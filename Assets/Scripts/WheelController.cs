365623aa768c78f3ec543c227ecea614a81b353e
Leo-Fetahu <100564927+Leo-Fetahu@users.noreply.github.com>
2023-04-24 12:39:57 +0100

    More progress on car controller, textmeshPro for ui

--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -17,6 +17,11 @@ public class WheelController : MonoBehaviour {
     public float acceleration = 500f;
     public float breakingForce = 300f;
     public float maxTurnAngle = 15f;
+    public float nitrousBoost = 1000f; // Amount of nitrous boost
+    public float nitrousDuration = 5f; // Duration of nitrous boost
+
+    private bool nitrousActivated = false; // Flag to track if nitrous is activated
+    private float nitrousTimer = 0f; // Timer to track duration of nitrous boost
 
     private float currentAcceleration = 0f;
     private float currentBreakForce = 0f;
@@ -24,45 +29,60 @@ public class WheelController : MonoBehaviour {
 
     private void FixedUpdate() {
 
-    // Get forward/reverse acceleration from the vertical axis (W and S keys)
-    currentAcceleration = acceleration * Input.GetAxis("Vertical");
+        // Get forward/reverse acceleration from the vertical axis (W and S keys)
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
 
-    // If we're pressing space, give currentBreakingForce a value.
-    if (Input.GetKey(KeyCode.Space))
-        currentBreakForce = breakingForce;
-    else
-        currentBreakForce = 0f;
+        // If we're pressing space, give currentBreakingForce a value.
+        if (Input.GetKey(KeyCode.Space))
+            currentBreakForce = breakingForce;
+        else
+            currentBreakForce = 0f;
 
-    //Apply acceleration to front wheels
-    frontRight.motorTorque = currentAcceleration;
-    frontLeft.motorTorque = currentAcceleration;
+        // Nitrous boost activation
+        if (Input.GetKeyDown(KeyCode.N) && !nitrousActivated) {
+            nitrousActivated = true;
+            nitrousTimer = 0f;
+        }
 
-    frontRight.brakeTorque = currentBreakForce;
-    frontLeft.brakeTorque = currentBreakForce;
-    backRight.brakeTorque = currentBreakForce;
-    backLeft.brakeTorque = currentBreakForce;
+        // Nitrous boost duration
+        if (nitrousActivated && nitrousTimer < nitrousDuration) {
+            currentAcceleration += nitrousBoost;
+            nitrousTimer += Time.deltaTime;
+        } else {
+            nitrousActivated = false;
+        }
 
-    //Take care of the steering.
-    currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
-    frontLeft.steerAngle = currentTurnAngle;
-    frontRight.steerAngle = currentTurnAngle;
+        // Apply acceleration to front wheels
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
 
-    //Update wheel meshes.
-    UpdateWheel(frontLeft, frontLeftTransform);
-    UpdateWheel(frontRight, frontRightTransform);
-    UpdateWheel(backLeft, backLeftTransform);
-    UpdateWheel(backRight, backRightTransform);
+        frontRight.brakeTorque = currentBreakForce;
+        frontLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
 
-}
+
+        // Take care of the steering.
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        // Update wheel meshes.
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(backLeft, backLeftTransform);
+        UpdateWheel(backRight, backRightTransform);
+
+    }
 
     void UpdateWheel(WheelCollider col, Transform trans) {
 
-        //Get wheel collider state.
+        // Get wheel collider state.
         Vector3 position;
         Quaternion rotation;
         col.GetWorldPose(out position, out rotation);
 
-        //Set wheel transform state.
+        // Set wheel transform state.
         trans.position = position;
         trans.rotation = rotation;
 
