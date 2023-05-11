57cb910c4bf7c8b97b528dcc7bd34365c3b2411b
Leo Fetahu <521935@escg.ac.uk>
2023-05-11 14:40:45 +0100

    Added speedometer function which fully works

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Speedometer : MonoBehaviour
 {
+    public Rigidbody target;
 
-    public Rigidbody theCar;
-    public float maxSpeed = 0.0f;
+    public float maxSpeed = 0.0f; // The maximum speed of the car ** in KM/H **
 
-    // Update is called once per frame
-    void Update()
+    public float minSpeedNeedleAngle;
+    public float maxSpeedNeedleAngle;
+
+    [Header("UI")]
+    public TextMeshProUGUI speedLabel; // The label that displays the speed
+    public RectTransform needle; // The needle in the speedometer
+
+    private float speed = 0.0f;
+
+    private void Update()
     {
-        
+        // 3.6f to convert to KM/H
+        speed = target.velocity.magnitude * 3.6f;
+
+        if (speedLabel != null)
+            speedLabel.text = ((int)speed) + " km/h";
+        if (needle != null)
+            needle.localEulerAngles = new Vector3(0, 0, Mathf.Lerp(minSpeedNeedleAngle, maxSpeedNeedleAngle, speed / maxSpeed));
     }
 }
